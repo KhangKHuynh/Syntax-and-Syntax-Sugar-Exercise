72d0ee75d59b97c4a7246434d96e6d4ca2fb6b2b
Khang Huynh <kkyhuynh@gmail.com>
2024-12-05 12:03:34 -0600

    assignment question

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -8,5 +8,8 @@
             var response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
             Console.WriteLine(response); 
         }
+        // not really understanding why my rider won't let me run this code? I tried redoing the fork/clone 
+        // Also looked over my code with Seth's code and it looks identical to me 
+        // Probably just a simple error somewhere just don't know :(
     }
 }
